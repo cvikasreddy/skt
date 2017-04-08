505a67f65d39e138a0fc9f50aec41c8214c36753
cvikasreddy <vikas.challaram@gmail.com>
2017-04-08 14:27:25 -0400

    Initial commmit with text data

---
+++ test.py
@@ -0,0 +1,239 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import tensorflow as tf
+import numpy as np
+import random
+#from tqdm import tqdm # ProgressBar for loops
+                                                                                                            
+from tensorflow.python.ops import rnn_cell, seq2seq
+from utils.data_loader import SKTDataLoader
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+
+
+# In[2]:
+
+num_layers = 1   # Number of layers of RNN
+num_hidden = 128 # Hidden size of RNN cell
+batch_size = 64 
+seq_length = 50  # Length of sequence
+num_outputs = 2  
+learning_rate = 0.0001
+num_epochs = 10
+
+model_name = 'skt'
+
+
+# In[3]:
+
+data_loader = SKTDataLoader('data/input_complete_split.txt', 'data/output_complete_split.txt',batch_size,seq_length)
+vocab_size =  data_loader.vocab_size   # Number of unique words in dataset
+
+data_size = data_loader.data_size      # Number of paris in the entire dataset
+train_set_size = data_loader.train_size# Number of pairs in train set
+valid_set_size = data_loader.valid_size# Number of pairs in valid set
+test_set_size = data_loader.test_size  # Number of pairs in test set
+
+num_train_batches = int(train_set_size*1.0/batch_size) # Number of train batches1
+num_valid_batches = int(valid_set_size*1.0/batch_size)
+num_test_batches = int(test_set_size*1.0/batch_size)
+
+print "Vocab Size: " + str(vocab_size)
+print "Data Size: " + str(data_size)
+print train_set_size, valid_set_size, test_set_size
+
+
+# In[4]:
+
+with tf.name_scope('encode_input'):
+    encode_input = [tf.placeholder(tf.int32, shape=(None,), name = "ei_%i" %i) for i in range(seq_length)]
+
+with tf.name_scope('labels'):
+    labels = [tf.placeholder(tf.int32, shape=(None,), name = "l_%i" %i) for i in range(seq_length)]
+
+with tf.name_scope('decode_input'):
+    decode_input = [tf.zeros_like(encode_input[0], dtype=np.int32, name="GO")] + labels[:-1]
+    
+with tf.name_scope('dropout'):
+    keep_prob = tf.placeholder("float", name='keep_prob')
+
+
+# In[5]:
+
+cells = [rnn_cell.DropoutWrapper(
+        rnn_cell.BasicLSTMCell(num_hidden), output_keep_prob=keep_prob
+    ) for i in range(num_layers)]
+
+stacked_lstm = rnn_cell.MultiRNNCell(cells)
+
+with tf.variable_scope("decoders") as scope:
+    decode_outputs, decode_state = seq2seq.embedding_rnn_seq2seq(
+        encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, 128)
+    
+#     decode_outputs, decode_state = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
+#         encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden)
+    
+    scope.reuse_variables()
+    
+#     decode_outputs, decode_state = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
+#         encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden, feed_previous=True)
+    
+    decode_outputs_test, decode_state_test = seq2seq.embedding_rnn_seq2seq(
+        encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, 128, feed_previous=True)
+
+
+# In[6]:
+
+with tf.name_scope('loss'):
+    loss_weights = [tf.ones_like(l, dtype=tf.float32) for l in labels]
+    loss = seq2seq.sequence_loss(decode_outputs, labels, loss_weights, vocab_size)
+
+tf.scalar_summary('loss', loss)
+
+
+# In[7]:
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+train = optimizer.minimize(loss)
+
+
+# In[8]:
+
+init = tf.initialize_all_variables()
+saver = tf.train.Saver()
+
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+#sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
+
+sess = tf.InteractiveSession()
+merged = tf.merge_all_summaries()
+summary_writer = tf.train.SummaryWriter('logs/' + model_name , sess.graph)
+
+saver.restore(sess, 'models/' + model_name)
+sess.run(init)
+
+
+# In[ ]:
+
+test_losses = []
+
+# Testing on test set
+for i in range(num_test_batches):
+    batch_inp, batch_outp = data_loader.next_batch(data_type='test')
+
+    input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
+    input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
+    input_dict[keep_prob] = 1.0
+
+    loss_val = sess.run(loss, feed_dict=input_dict)
+    test_losses.append(loss_val)
+
+
+log_txt = "Test_loss: " + str(round(np.mean(test_losses), 4)) + '+' + str(round(np.std(test_losses), 2)) 
+print log_txt
+
+f = open('log.txt', 'a')
+f.write(log_txt + '\n')
+f.close()
+
+
+# ### Getting output and analysing
+
+# 1. get batch data like batch_q1, batch_q2
+# 2. get outputs from decode_outputs_test and argmax
+# 3. use swapaxes to change it to [batch_size, seq_length] for all batch_q1, batch_q2, output
+# 4. use data_loader.idx2word to generate words for all batch_q1, batch_q2, output
+
+# In[10]:
+
+batch_inp, batch_outp = data_loader.next_batch()
+
+input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
+input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
+input_dict[keep_prob] = 1.0
+
+loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
+
+
+# In[11]:
+
+decoded_outputs = np.array(outputs).transpose([1,0,2])
+decoded_outputs = np.argmax(outputs, axis = 2)
+
+inps = np.swapaxes(batch_inp, 0, 1)
+outps = np.swapaxes(batch_outp, 0, 1)
+gens = np.swapaxes(decoded_outputs, 0, 1)
+
+
+# In[13]:
+
+index = random.randint(0, batch_size-1)
+
+inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != 14681]).replace('\xe2\x96\x81', ' ')
+outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != 14681]).replace('\xe2\x96\x81', ' ')
+gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != 14681]).replace('\xe2\x96\x81', ' ')
+
+
+# In[14]:
+
+print inp
+print outp
+
+
+# In[15]:
+
+print gen
+
+
+# #### Inputting the previous word for decoder
+
+# In[16]:
+
+batch_inp, batch_outp = data_loader.next_batch()
+
+input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
+input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
+input_dict[keep_prob] = 1.0
+
+#loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
+loss_val, outputs = sess.run([loss, decode_outputs], feed_dict = input_dict)
+
+
+# In[17]:
+
+decoded_outputs = np.array(outputs).transpose([1,0,2])
+decoded_outputs = np.argmax(outputs, axis = 2)
+
+inps = np.swapaxes(batch_inp, 0, 1)
+outps = np.swapaxes(batch_outp, 0, 1)
+gens = np.swapaxes(decoded_outputs, 0, 1)
+
+
+# In[18]:
+
+index = random.randint(0, batch_size-1)
+
+inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != 14681]).replace('\xe2\x96\x81', ' ')
+outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != 14681]).replace('\xe2\x96\x81', ' ')
+gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != 14681]).replace('\xe2\x96\x81', ' ')
+
+
+# In[19]:
+
+print inp
+print outp
+
+
+# In[20]:
+
+print gen
+
+
+# In[ ]:
+
+
+
---
+++ utils/data_loader.py
@@ -0,0 +1,201 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import os
+import numpy as np
+import pandas as pd
+from random import shuffle
+from tqdm import tqdm
+
+
+# In[2]:
+
+class SKTDataLoader(object):
+    """
+        What it does:
+            - tokenizes the entire dataset and saves
+            - creates the vocabulary of word in data
+            - indexes the words in vocabulary
+            - splits data into train, valid, test sets
+            - can return batch_data of specified type(train/valid/test)
+            
+        TODO: Replace less frequent words with a special word and if new word during test => treat it as the special word
+    """
+    def __init__(self, input_file_path, output_file_path, batch_size = 64, seq_length = 150, split = [0.70, 0.15, 0.15]):
+        self.input_file_path = input_file_path
+        self.output_file_path = output_file_path
+        self.base_path = os.path.dirname(self.input_file_path) + '/' + os.path.basename(self.input_file_path).split('.')[0] 
+        
+        self.input_data = open(input_file_path, 'r').readlines()
+        self.output_data = open(output_file_path, 'r').readlines()
+       
+        self.train_index = 0
+        self.cur_index = {}
+        self.cur_index['train'] = 0
+        self.cur_index['valid'] = 0
+        self.cur_index['test'] = 0
+        
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+        self.split = split
+        
+        self.complete_data = self.get_complete_data()
+        
+        # Get vocab, idx2word, word2idx i.e., all index all words in the data
+        self.vocab_dict = self.vocab()
+        self.idx2word, self.word2idx = self.index_vocab()
+        
+        self.go_index = len(self.word2idx) # Index of the word that is to be prepended to make sentences = 'seq_length'
+        self.vocab_size = len(self.vocab_dict) + 1 # 1 for filling the sequences which are smaller than 'seq_length'
+        
+        self.data = self.make_data() # Creates a list self.data which has data which statisfies seq_length condition
+        self.data_size = len(self.data)
+        self.data_set = self.split_data() # Splits data into train, valid, test sets and creates a dict self.data_set
+        
+        self.train_size = len(self.data_set['train'])
+        self.valid_size = len(self.data_set['valid'])
+        self.test_size = len(self.data_set['test'])
+        
+    def get_complete_data(self):
+        """
+            Tokenizes every question and save the entire data.
+        """
+        self.data_path = self.base_path + '_data.npy' 
+        if os.path.isfile(self.data_path): # If the indices file is present => load 
+            complete_data = np.load(self.data_path)
+            print "Loaded complete data"
+            return complete_data
+            
+        complete_data = []  
+        for inp, outp in tqdm(zip(self.input_data, self.output_data)):
+            inp = inp.split()
+            outp = outp.split()
+            complete_data.append([inp, outp])
+        np.save(self.data_path, complete_data)
+        print "Saved and loaded complete data"
+        return complete_data
+        
+    def index_vocab(self):
+        """
+            Makes dicts(idx2word, word2idx) from vocab_dict
+        """
+        idx2word = {}
+        word2idx = {}
+        word2idx_path = self.base_path + '_word2idx.npy'
+        idx2word_path = self.base_path + '_idx2word.npy'
+        
+        if os.path.isfile(word2idx_path) and os.path.isfile(idx2word_path): # If the vocab file is present => load and return only list of words
+            word2idx = np.load(word2idx_path).item()
+            idx2word = np.load(idx2word_path).item()
+            print "Loaded word2idx and idx2word"
+            return idx2word, word2idx
+        
+        for i, word in tqdm(enumerate(self.vocab_dict)):
+            word2idx[word] = i
+            idx2word[i] = word
+        np.save(word2idx_path, word2idx)
+        np.save(idx2word_path, idx2word)
+        print "Saved and loaded word2idx and idx2word"
+        return idx2word, word2idx
+                
+    def vocab(self):
+        """
+            Creates a dict vocab, which has all words and their occurence counts.
+            Returns the list of all words in the dataset in alphabetical order.
+        """
+        self.vocab_path = self.base_path + '_vocab.npy'
+        if os.path.isfile(self.vocab_path): # If the vocab file is present => load and return only list of words
+            vocab = np.load(self.vocab_path)
+            vocab_list = vocab.item().keys()
+            vocab_list.sort()
+            print "Loaded vocab"
+            return vocab_list
+        vocab = {}
+        for inp, outp in tqdm(self.complete_data): # uses all the data to create vocab
+            words = inp + outp
+            for word in words:
+                if word in vocab.keys():
+                    vocab[word] += 1
+                else:
+                    vocab[word] = 1
+        np.save(self.vocab_path, vocab)
+        vocab_list = vocab.keys()
+        vocab_list.sort()
+        print "Saved and Loaded vocab"
+        return vocab_list
+    
+    def make_data(self):
+        """
+            Uses the complete_data and samples data that statisfies seq_length
+        """
+        data = []
+        for inp, outp in self.complete_data:
+            if(len(inp) <= self.seq_length  and len(outp) <= self.seq_length):
+                data.append([inp, outp])
+        return data
+    
+    def split_data(self):
+        """
+            Splits data into train, valid, test sets
+        """
+        # Split the data into train, valid, test using a random data_indices
+        self.data_indices_path = self.base_path + '_data_indices.npy'
+        if os.path.isfile(self.data_indices_path): # If the indices file is present => load 
+            self.data_indices = np.load(self.data_indices_path)
+        else: # Else create indices now and store
+            self.data_indices = range(self.data_size)
+            shuffle(self.data_indices)
+            np.save(self.data_indices_path, self.data_indices)
+            
+        # Splitting indices and making train, valid, test data according to their ratio
+        self.train_indices = self.data_indices[:int(self.data_size*self.split[0])]
+        self.valid_indices = self.data_indices[int(self.data_size*self.split[0]):int(self.data_size*(self.split[0] + self.split[1]))]
+        self.test_indices = self.data_indices[int(self.data_size*(self.split[0] + self.split[1])):int(self.data_size*(self.split[0] + self.split[1] + self.split[2]))]
+               
+        self.train_data = [self.data[x] for x in self.train_indices]
+        self.valid_data = [self.data[x] for x in self.valid_indices]
+        self.test_data = [self.data[x] for x in self.test_indices]
+        
+        print "Created dataset"
+        return {'train': self.train_data, 'valid': self.valid_data, 'test': self.test_data}
+                
+    def reset_index(self, data_type='train'):
+        self.cur_index[data_type] = 0
+        
+    def decode_sentence(self, list_of_ids):
+        return [self.idx2word[x] for x in list_of_ids]
+        
+    def encode_sentence(self, sentence):
+        """
+            Takes in a list of words and encodes them using word2id.
+        """
+        return self.prepend_sentence([self.word2idx[x] for x in sentence])
+    
+    def encode_batch(self, batch_data):
+        batch_inp = []
+        batch_outp = []
+        for pair in batch_data:
+            batch_inp.append(self.encode_sentence(pair[0]))
+            batch_outp.append(self.encode_sentence(pair[1]))
+        return batch_inp, batch_outp
+    
+    def prepend_sentence(self, list_of_ids):
+        """
+            Prepends a list of ids with a id that is not present.
+        """
+        return [self.go_index]*(self.seq_length - len(list_of_ids)) + list_of_ids
+    
+    def next_batch(self, data_type='train'):
+        """
+            Returns batch_data of size 'batch_size' and of corresponding 'data_type'
+        """
+        stop = False
+        if(self.cur_index[data_type] + self.batch_size > self.data_size):
+            self.reset_index(data_type = data_type)
+            stop = True
+        batch_data = self.data_set[data_type][self.cur_index[data_type] : self.cur_index[data_type] + self.batch_size]
+        batch_data = self.encode_batch(batch_data)
+        
+        return np.swapaxes(np.array(batch_data[0]), 0, 1), np.swapaxes(np.array(batch_data[1]), 0, 1)
