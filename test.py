05b8822905f0055ea45dfd464adb81b547462a85
Vikas Reddy <vikas.challaram@gmail.com>
2017-10-04 14:48:54 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -2,36 +2,34 @@
 # coding: utf-8
 
 # In[1]:
-
+																											
 import tensorflow as tf
 import numpy as np
 import random
-#from tqdm import tqdm # ProgressBar for loops
-                                                                                                            
+from tqdm import tqdm # ProgressBar for loops
+																											
 from tensorflow.python.ops import rnn_cell, seq2seq
 from utils.data_loader import SKTDataLoader
 
-#import os
-#os.environ["CUDA_VISIBLE_DEVICES"]=""
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"]=""
 
 
 # In[2]:
 
-num_layers = 1   # Number of layers of RNN
+num_layers = 3   # Number of layers of RNN
 num_hidden = 128  # Hidden size of RNN cell
-batch_size = 64  # Number of sentences in a batch
-seq_length = 50  # Length of sequence
-split = [0.90, 0.05, 0.05] # Splitting proportions into train, valid, test
+batch_size = 1  # Number of sentences in a batch
+seq_length = 35  # Length of sequence
+split = [0, 0, 1] # Splitting proportions into train, valid, test
 learning_rate = 0.001
-num_epochs = 10
-verbose = 1      # Display every <verbose> epochs
 
-model_name = 'skt'
+model_name = 'attn_3_8000_0.8_trainonly' # Name is <num_layers>_<sentencepiece_vocabsize>_<keep_prob>
 
 
 # In[3]:
 
-data_loader = SKTDataLoader('data/input_complete_100_split.txt','data/output_complete_100_split.txt',batch_size,seq_length, split=split)
+data_loader = SKTDataLoader('data/dcs_data_input_test_sent.txt','data/dcs_data_output_test_sent.txt',batch_size,seq_length, split=split)
 vocab_size =  data_loader.vocab_size   # Number of unique words in dataset
 
 data_size = data_loader.data_size      # Number of paris in the entire dataset
@@ -51,47 +49,39 @@ print train_set_size, valid_set_size, test_set_size
 # In[4]:
 
 with tf.name_scope('encode_input'):
-    encode_input = [tf.placeholder(tf.int32, shape=(None,), name = "ei_%i" %i) for i in range(seq_length)]
+	encode_input = [tf.placeholder(tf.int32, shape=(None,), name = "ei_%i" %i) for i in range(seq_length)]
 
 with tf.name_scope('labels'):
-    labels = [tf.placeholder(tf.int32, shape=(None,), name = "l_%i" %i) for i in range(seq_length)]
+	labels = [tf.placeholder(tf.int32, shape=(None,), name = "l_%i" %i) for i in range(seq_length)]
 
 with tf.name_scope('decode_input'):
-    decode_input = [tf.zeros_like(encode_input[0], dtype=np.int32, name="GO")] + labels[:-1]
-    
+	decode_input = [tf.zeros_like(encode_input[0], dtype=np.int32, name="GO")] + labels[:-1]
+	
 with tf.name_scope('dropout'):
-    keep_prob = tf.placeholder("float", name='keep_prob')
+	keep_prob = tf.placeholder("float", name='keep_prob')
 
 
 # In[5]:
 
 cells = [rnn_cell.DropoutWrapper(
-        rnn_cell.BasicLSTMCell(num_hidden), output_keep_prob=keep_prob
-    ) for i in range(num_layers)]
+		rnn_cell.BasicLSTMCell(num_hidden), output_keep_prob=keep_prob
+	) for i in range(num_layers)]
 
 stacked_lstm = rnn_cell.MultiRNNCell(cells)
 
 with tf.variable_scope("decoders") as scope:
-    decode_outputs, decode_state = seq2seq.embedding_rnn_seq2seq(
-        encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, 128)
-    
-#     decode_outputs, decode_state = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
-#         encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden)
-    
-    scope.reuse_variables()
-    
-#     decode_outputs, decode_state = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
-#         encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden, feed_previous=True)
-    
-    decode_outputs_test, decode_state_test = seq2seq.embedding_rnn_seq2seq(
-        encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, 128, feed_previous=True)
+	decode_outputs, decode_state = seq2seq.embedding_attention_seq2seq(encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden)
 
+    scope.reuse_variables()
+
+    decode_outputs_test, decode_state_test = seq2seq.embedding_attention_seq2seq(encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden, feed_previous=True)
+    
 
 # In[6]:
 
 with tf.name_scope('loss'):
-    loss_weights = [tf.ones_like(l, dtype=tf.float32) for l in labels]
-    loss = seq2seq.sequence_loss(decode_outputs, labels, loss_weights, vocab_size)
+	loss_weights = [tf.ones_like(l, dtype=tf.float32) for l in labels]
+	loss = seq2seq.sequence_loss(decode_outputs, labels, loss_weights, vocab_size)
 
 tf.scalar_summary('loss', loss)
 
@@ -107,135 +97,126 @@ train = optimizer.minimize(loss)
 init = tf.initialize_all_variables()
 saver = tf.train.Saver()
 
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
-#sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
-
 sess = tf.InteractiveSession()
 merged = tf.merge_all_summaries()
 summary_writer = tf.train.SummaryWriter('logs/' + model_name , sess.graph)
 
-saver.restore(sess, 'models/' + model_name)
 sess.run(init)
+saver.restore(sess, 'models/' + model_name)
 
-
-# In[ ]:
-
-# test_losses = []
-
-# # Testing on test set
-# for i in range(num_test_batches):
-#     batch_inp, batch_outp = data_loader.next_batch(data_type='test')
-
-#     input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-#     input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-#     input_dict[keep_prob] = 1.0
-
-#     loss_val = sess.run(loss, feed_dict=input_dict)
-#     test_losses.append(loss_val)
-
-
-# log_txt = "Test_loss: " + str(round(np.mean(test_losses), 4)) + '+' + str(round(np.std(test_losses), 2)) 
-# print log_txt
-
-# f = open('log.txt', 'a')
-# f.write(log_txt + '\n')
-# f.close()
-
-
-# ### Getting output and analysing
-
-# 1. get batch data like batch_inp, batch_outp
-# 2. get outputs from decode_outputs_test and argmax
-# 3. use swapaxes to change it to [batch_size, seq_length] for all batch_q1, batch_q2, output
-# 4. use data_loader.idx2word to generate words for all batch_q1, batch_q2, output
 
 # In[10]:
 
-batch_inp, batch_outp = data_loader.next_batch()
+test_losses = []
 
-input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-input_dict[keep_prob] = 1.0
+# Testing on test set
+for i in range(num_test_batches):
+	batch_inp, batch_outp = data_loader.next_batch(data_type='test')
 
-loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
+	input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
+	input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
+	input_dict[keep_prob] = 1.0
 
-
-# In[11]:
-
-decoded_outputs = np.array(outputs).transpose([1,0,2])
-decoded_outputs = np.argmax(outputs, axis = 2)
-
-inps = np.swapaxes(batch_inp, 0, 1)
-outps = np.swapaxes(batch_outp, 0, 1)
-gens = np.swapaxes(decoded_outputs, 0, 1)
+	loss_val = sess.run(loss, feed_dict=input_dict)
+	test_losses.append(loss_val)
 
 
-# In[13]:
+log_txt = "Test_loss: " + str(round(np.mean(test_losses), 4)) + '+' + str(round(np.std(test_losses), 2)) 
+print log_txt
 
-index = random.randint(0, batch_size-1)
-
-inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1]).replace('\xe2\x96\x81', ' ')
-outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1][::-1]).replace('\xe2\x96\x81', ' ')
-gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1][::-1]).replace('\xe2\x96\x81', ' ')
-
-
-# In[14]:
-
-print inp
-print outp
+f = open('log.txt', 'a')
+f.write(log_txt + '\n')
+f.close()
 
 
-# In[15]:
+# ### Calculating precision, recall
 
-print gen
-print
-print
+# #### Getting outputs on entire test set
 
-# #### Inputting the previous word for decoder
+# In[ ]:
 
-# In[16]:
+data_loader.reset_index(data_type='test')
 
-batch_inp, batch_outp = data_loader.next_batch()
+X_test = []
+y_test = []
+y_out = []
 
-input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-input_dict[keep_prob] = 1.0
+for i in tqdm(range(num_test_batches)):
+	
+	batch_inp, batch_outp = data_loader.next_batch(data_type='test')
 
-#loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
-loss_val, outputs = sess.run([loss, decode_outputs], feed_dict = input_dict)
+	input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
+	input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
+	input_dict[keep_prob] = 1.0
 
+	loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
 
-# In[17]:
+	decoded_outputs = np.array(outputs).transpose([1,0,2])
+	decoded_outputs = np.argmax(outputs, axis = 2)
 
-decoded_outputs = np.array(outputs).transpose([1,0,2])
-decoded_outputs = np.argmax(outputs, axis = 2)
+	inps = np.swapaxes(batch_inp, 0, 1)
+	outps = np.swapaxes(batch_outp, 0, 1)
+	gens = np.swapaxes(decoded_outputs, 0, 1)
 
-inps = np.swapaxes(batch_inp, 0, 1)
-outps = np.swapaxes(batch_outp, 0, 1)
-gens = np.swapaxes(decoded_outputs, 0, 1)
+	for index in range(batch_size):
+		inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1][::-1])
+		outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1])
+		gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1])
 
-
-# In[18]:
-
-index = random.randint(0, batch_size-1)
-
-inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1]).replace('\xe2\x96\x81', ' ')
-outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1]).replace('\xe2\x96\x81', ' ')
-gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1]).replace('\xe2\x96\x81', ' ')
-
-
-# In[19]:
-
-print inp
-print outp
-
-
-# In[20]:
-
-print gen
+		X_test.append(inp.split('\xe2\x96\x81'))
+		y_test.append(outp.split('\xe2\x96\x81'))
+		y_out.append(gen.split('\xe2\x96\x81'))
 
 
 # In[ ]:
 
+precisions = []
+recalls = []
+accuracies = []
+
+f = open('output_3_attn_1_do_0.3.txt', 'a')
+
+for inp, outp, gen in zip(X_test, y_test, y_out):
+
+	inp_raw = ' '.join(inp)
+	outp_raw = ' '.join(outp)
+	gen_raw = ' '.join(gen)
+
+	intersection = set(outp).intersection(gen)
+	prec = len(intersection)*1.0/len(gen)
+	recall = len(intersection)*1.0/len(outp)
+
+	if outp == gen:
+		accuracies.append(1.0)
+	else:
+		accuracies.append(0.0)
+	
+	precisions.append(prec)
+	recalls.append(recall)
+
+	# print inp_raw
+	# print outp_raw
+	# print gen_raw
+	# print prec
+	# print recall
+
+	log_line = str(inp_raw).replace('\n', '').lstrip() + ';' + str(outp_raw).replace('\n', '').lstrip() + ';' + str(gen_raw).replace('\n', '').lstrip() + ';' + str(prec).replace('\n', '') + ';' + str(recall).replace('\n', '') + '\n'
+
+	f.write(log_line)
+
+f.close()
+
+# In[ ]:
+
+avg_prec = np.mean(precisions)*100.0
+avg_recall = np.mean(recalls)*100.0
+f1_score = 2*avg_prec*avg_recall/(avg_prec + avg_recall)
+avg_acc = np.mean(accuracies)
 
 
+# In[ ]:
+
+print "Precision: " + str(avg_prec) 
+print "Recall: " + str(avg_recall)
+print "F1_score: " + str(f1_score)
+print "Accuracy: " + str(avg_acc)
--- train.py
+++ train.py
@@ -17,21 +17,21 @@ from utils.data_loader import SKTDataLoader
 
 # In[2]:
 
-num_layers = 3   # Number of layers of RNN
-num_hidden = 128  # Hidden size of RNN cell
-batch_size = 64  # Number of sentences in a batch
-seq_length = 50  # Length of sequence
-split = [0.90, 0.05, 0.05] # Splitting proportions into train, valid, test
-learning_rate = 0.001
-num_epochs = 30
+num_layers = 3 # Number of layers of RNN
+num_hidden = 128 # Hidden size of RNN cell
+batch_size = 128 # Number of sentences in a batch
+seq_length = 35 # Length of sequence
+split = [0.9, 0.1, 0] # Splitting proportions into train, valid, test
+learning_rate = 0.001 # Initial learning rate
+keep_prob_val = 0.8 # keep_prob is 1 - dropout i.e., if dropout = 0.2, then keep_prob is 0.8
+num_epochs = 100
 verbose = 1      # Display every <verbose> epochs
 
-model_name = 'skt_3_1000' # Name is <skt>_<num_layers>_<sentencepiece_vocabsize>
+model_name = 'attn_3_8000_0.8_trainonly' # Name is <num_layers>_<sentencepiece_vocabsize>_<keep_prob>
 
 
 # In[3]:
-
-data_loader = SKTDataLoader('data/input_complete_1000_split.txt','data/output_complete_1000_split.txt',batch_size,seq_length, split=split)
+data_loader = SKTDataLoader('data/dcs_data_input_train_sent.txt','data/dcs_data_output_train_sent.txt',batch_size,seq_length, split=split)
 vocab_size =  data_loader.vocab_size   # Number of unique words in dataset
 
 data_size = data_loader.data_size      # Number of paris in the entire dataset
@@ -72,29 +72,18 @@ cells = [rnn_cell.DropoutWrapper(
 stacked_lstm = rnn_cell.MultiRNNCell(cells)
 
 with tf.variable_scope("decoders") as scope:
-    decode_outputs, decode_state = seq2seq.embedding_rnn_seq2seq(
-        encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, embedding_size=num_hidden)
-    
-#     decode_outputs, decode_state = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
-#         encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden)
-    
-    scope.reuse_variables()
-    
-#     decode_outputs, decode_state = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
-#         encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden, feed_previous=True)
-    
-    decode_outputs_test, decode_state_test = seq2seq.embedding_rnn_seq2seq(
-        encode_input, decode_input, stacked_lstm,vocab_size,vocab_size,embedding_size=num_hidden,feed_previous=True)
+    decode_outputs, decode_state = seq2seq.embedding_attention_seq2seq(encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden)
 
+    scope.reuse_variables()
+
+    decode_outputs_test, decode_state_test = seq2seq.embedding_attention_seq2seq(encode_input, decode_input, stacked_lstm, vocab_size, vocab_size, num_hidden, feed_previous=True)
+    
 
 # In[6]:
 
 with tf.name_scope('loss'):
     loss_weights = [tf.ones_like(l, dtype=tf.float32) for l in labels]
     loss = seq2seq.sequence_loss(decode_outputs, labels, loss_weights, vocab_size)
-
-#with tf.name_scope('accuracy'):
-    #macro_accuracy = 
 
 tf.scalar_summary('loss', loss)
 
@@ -110,15 +99,12 @@ train = optimizer.minimize(loss)
 init = tf.initialize_all_variables()
 saver = tf.train.Saver()
 
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
-#sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
-
 sess = tf.InteractiveSession()
 merged = tf.merge_all_summaries()
 summary_writer = tf.train.SummaryWriter('logs/' + model_name , sess.graph)
 
-#saver.restore(sess, 'models/' + model_name)
 sess.run(init)
+#saver.restore(sess, 'models/' + model_name)
 
 
 # In[9]:
@@ -130,12 +116,12 @@ try:
         valid_losses = []
 
         # Training on train set
-        for i in range(num_train_batches):
+        for i in tqdm(range(num_train_batches)):
             batch_inp, batch_outp = data_loader.next_batch()
 
             input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
             input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-            input_dict[keep_prob] = 1.0
+            input_dict[keep_prob] = keep_prob_val
 
             _, loss_val, summary = sess.run([train, loss, merged], feed_dict=input_dict)
             train_losses.append(loss_val)
@@ -155,7 +141,7 @@ try:
             valid_losses.append(loss_val)
 
         if epoch % verbose == 0:
-            log_txt = "Epoch: " + str(epoch) + " Steps: " + str(step) +                 " train_loss: " + str(round(np.mean(train_losses),4)) + '+' + str(round(np.std(train_losses),2)) +                 " valid_loss: " + str(round(np.mean(valid_losses),4)) + '+' + str(round(np.std(valid_losses),2)) 
+            log_txt = "Epoch: " + str(epoch) + " Steps: " + str(step) + " train_loss: " + str(round(np.mean(train_losses),4)) + '+' + str(round(np.std(train_losses),2)) +                 " valid_loss: " + str(round(np.mean(valid_losses),4)) + '+' + str(round(np.std(valid_losses),2)) 
             print log_txt
 
             f = open('log.txt', 'a')
@@ -166,229 +152,4 @@ try:
 except KeyboardInterrupt:
     print "Stopped at epoch: " + str(epoch) + ' and step: ' + str(step)
 
-
-# In[10]:
-
-test_losses = []
-
-# Testing on test set
-for i in range(num_test_batches):
-    batch_inp, batch_outp = data_loader.next_batch(data_type='test')
-
-    input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-    input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-    input_dict[keep_prob] = 1.0
-
-    loss_val = sess.run(loss, feed_dict=input_dict)
-    test_losses.append(loss_val)
-
-
-log_txt = "Test_loss: " + str(round(np.mean(test_losses), 4)) + '+' + str(round(np.std(test_losses), 2)) 
-print log_txt
-
-f = open('log.txt', 'a')
-f.write(log_txt + '\n')
-f.close()
-
-
-# ### Getting output and analysing
-
-# 1. get batch data like batch_inp, batch_outp
-# 2. get outputs from decode_outputs_test and argmax
-# 3. use swapaxes to change it to [batch_size, seq_length] for all batch_inp, batch_outp, output
-# 4. use data_loader.idx2word to generate words for all batch_inp, batch_outp, output
-
-# In[13]:
-
-batch_inp, batch_outp = data_loader.random_batch()
-
-input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-input_dict[keep_prob] = 1.0
-
-loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
-
-
-# In[11]:
-
-decoded_outputs = np.array(outputs).transpose([1,0,2])
-decoded_outputs = np.argmax(outputs, axis = 2)
-
-inps = np.swapaxes(batch_inp, 0, 1)
-outps = np.swapaxes(batch_outp, 0, 1)
-gens = np.swapaxes(decoded_outputs, 0, 1)
-
-
-# In[12]:
-
-#index = random.randint(0, batch_size-1)
-for index in range(10):
-    inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1][::-1])#.replace('\xe2\x96\x81', ' ')
-    outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1])#.replace('\xe2\x96\x81', ' ')
-    gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1])#.replace('\xe2\x96\x81', ' ')
-    
-    inp_raw = inps[index]
-    outp_raw = outps[index]
-    gen_raw = gens[index]
-    
-    
-    print inp
-    print outp
-    print gen
-    
-#     print [x for x in inp_raw[::-1] if x != vocab_size-1]
-#     print [x for x in outp_raw if x != vocab_size-1]
-#     print [x for x in gen_raw if x != vocab_size-1]
-    print 
-
-
-# #### Inputting the previous word for decoder
-
-# In[13]:
-
-batch_inp, batch_outp = data_loader.random_batch()
-
-input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-input_dict[keep_prob] = 1.0
-
-#loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
-loss_val, outputs = sess.run([loss, decode_outputs], feed_dict = input_dict)
-
-
-# In[14]:
-
-decoded_outputs = np.array(outputs).transpose([1,0,2])
-decoded_outputs = np.argmax(outputs, axis = 2)
-
-inps = np.swapaxes(batch_inp, 0, 1)
-outps = np.swapaxes(batch_outp, 0, 1)
-gens = np.swapaxes(decoded_outputs, 0, 1)
-
-
-# In[15]:
-
-#index = random.randint(0, batch_size-1)
-for index in range(10):
-    inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1][::-1])#.replace('\xe2\x96\x81', ' ')
-    outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1][::-1])#.replace('\xe2\x96\x81', ' ')
-    gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1][::-1])#.replace('\xe2\x96\x81', ' ')
-    
-    print inp
-    print outp
-    print gen
-    print
-
-
-# #### Checking for test data
-
-# In[16]:
-
-batch_inp, batch_outp = data_loader.random_batch(data_type='test')
-
-input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-input_dict[keep_prob] = 1.0
-
-#loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
-loss_val, outputs = sess.run([loss, decode_outputs], feed_dict = input_dict)
-
-
-# In[17]:
-
-decoded_outputs = np.array(outputs).transpose([1,0,2])
-decoded_outputs = np.argmax(outputs, axis = 2)
-
-inps = np.swapaxes(batch_inp, 0, 1)
-outps = np.swapaxes(batch_outp, 0, 1)
-gens = np.swapaxes(decoded_outputs, 0, 1)
-
-
-# In[20]:
-
-#index = random.randint(0, batch_size-1)
-for index in range(10):
-    inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1][::-1])#.replace('\xe2\x96\x81', ' ')
-    outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1])#.replace('\xe2\x96\x81', ' ')
-    gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1])#.replace('\xe2\x96\x81', ' ')
-    
-    inp_raw = inps[index]
-    outp_raw = outps[index]
-    gen_raw = gens[index]
-       
-    print inp
-    print outp
-    print gen
-    
-#     print [x for x in inp_raw[::-1] if x != vocab_size-1]
-#     print [x for x in outp_raw if x != vocab_size-1]
-#     print [x for x in gen_raw if x != vocab_size-1]
-    print
-
-
-# ### Calculating precision, recall
-
-# #### Getting outputs on entire test set
-
-# In[ ]:
-
-data_loader.reset_index(data_type='test')
-
-X_test = []
-y_test = []
-y_out = []
-
-for i in range(num_test_batches):
-    
-    batch_inp, batch_outp = data_loader.next_batch(data_type='test')
-
-    input_dict = {encode_input[t]: batch_inp[t] for t in range(seq_length)}
-    input_dict.update({labels[t]: batch_outp[t] for t in range(seq_length)})
-    input_dict[keep_prob] = 1.0
-
-    loss_val, outputs = sess.run([loss, decode_outputs_test], feed_dict = input_dict)
-
-    decoded_outputs = np.array(outputs).transpose([1,0,2])
-    decoded_outputs = np.argmax(outputs, axis = 2)
-
-    inps = np.swapaxes(batch_inp, 0, 1)
-    outps = np.swapaxes(batch_outp, 0, 1)
-    gens = np.swapaxes(decoded_outputs, 0, 1)
-
-    for index in range(batch_size):
-        inp = ''.join([data_loader.idx2word[x] for x in inps[index] if x != vocab_size-1][::-1])
-        outp = ''.join([data_loader.idx2word[x] for x in outps[index] if x != vocab_size-1])
-        gen = ''.join([data_loader.idx2word[x] for x in gens[index] if x != vocab_size-1])
-
-        X_test.append(inp.split())
-        y_test.append(outp.split())
-        y_out.append(gen.split())
-
-
-# In[ ]:
-
-precisions = []
-recalls = []
-
-for outp, gen in zip(y_test, y_out):
-    intersection = set(outp).intersection(gen)
-    prec = intersection*1.0/len(gen)
-    recall = intersection*1.0/len(outp)
-    
-    precisions.append(prec)
-    recalls.append(recall)
-
-
-# In[ ]:
-
-avg_prec = np.mean(precisions)*100.0
-avg_recall = np.mean(recalls)*100.0
-f1_score = 2*avg_prec*avg_recall/(avg_prec + avg_recall)
-
-
-# In[ ]:
-
-print "Precision: " + str(avg_prec) 
-print "Recall: " + str(avg_recall)
-print "F1_score: " + str(f1_score)
-
+print "Training completed"
